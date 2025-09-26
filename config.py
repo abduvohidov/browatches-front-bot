9ebf1b816af03596572d5a9eb468366e7638e1f9
Abdullah <abduvohidov.abdullokh@gmail.com>
2025-09-26 10:30:10 +0500

    Update configuration with new bot token and Google Sheets URL

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = "8251895501:AAGoJ3E8jnOtSL_triH1sWDeH6Wdx--8xU4"
+BOT_TOKEN = "8411883721:AAEinMPpYwWWKoDDsT1dBbs2Uuao3xA7HS0"
 ADMIN_CHAT_ID = 1247573660
-GOOGLE_SHEETS_URL = "https://docs.google.com/spreadsheets/d/1pIWAwVRL3YC603p_8JxF5827AbmIYCRddsoXZpQshVI/edit?usp=sharing"
+GOOGLE_SHEETS_URL = "https://docs.google.com/spreadsheets/d/1QmBN04iLm4NlxXeKzWxe7QLCglmP9RtUM_eMVc2b8uY/edit?usp=sharing"
 
 # Настройки веб-сервера
 WEB_SERVER_PORT = 3000
